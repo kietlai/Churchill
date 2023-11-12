95ba248c575ce5c3c6263d1cb70c85690ae44132
Hero E <heroemenalom73@gmail.com>
2023-11-12 09:19:15 -0500

    added signUpWithEmail to client.ts; changed sign-in route to sign-up; added test code in /sign-up

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,19 @@
 // supabase
 import { createClient } from '@supabase/supabase-js'
+import { sign } from 'crypto'
 
 const client = createClient('https://hfzmknwnkgiibhmyvbwo.supabase.co',process.env.SUPABASE_KEY as string)
+
+export const signUpWithEmail = async (email: string, pass: string) => {
+    
+    await client.auth.signUp({
+        email: email,
+        password: pass,
+        options: {
+            emailRedirectTo: '/'
+        }
+    })
+    
+}
 
 export default client;
