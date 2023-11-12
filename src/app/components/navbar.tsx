a078cc1f96ad4244888833924986464c324c4caa
Hero E <heroemenalom73@gmail.com>
2023-11-12 11:43:50 -0500

    installed supbase auth helpers for nextjs (package.json)

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -15,6 +15,7 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, RectangleGroupIcon } from '@heroicons/react/20/solid'
 import { Logo } from './logo'
+import client from '../client'
 
 const products = [
   {
@@ -50,14 +51,15 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar() {
+export default async function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
 
   return (
     <header className="relative isolate z-10 bg-white border-bottom-8 border-gray-400">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <Logo width={175} height={100} />
           </a>
