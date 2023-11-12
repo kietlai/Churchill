351629fe874a0317053c61e3d98ef26ef5c6172d
Hero E <heroemenalom73@gmail.com>
2023-11-12 09:23:09 -0500

    added comments to client.ts

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,8 +2,10 @@
 import { createClient } from '@supabase/supabase-js'
 import { sign } from 'crypto'
 
+// supabase client to access database
 const client = createClient('https://hfzmknwnkgiibhmyvbwo.supabase.co',process.env.SUPABASE_KEY as string)
 
+// Make a new account using email
 export const signUpWithEmail = async (email: string, pass: string) => {
     
     await client.auth.signUp({
@@ -13,7 +15,7 @@ export const signUpWithEmail = async (email: string, pass: string) => {
             emailRedirectTo: '/'
         }
     })
-    
+
 }
 
 export default client;
