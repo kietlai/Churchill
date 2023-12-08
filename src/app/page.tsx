91bc8a994062b6a2dc79760a2db35c3efb9904e9
Hero Emenalom <heroemenalom73@gmail.com>
2023-12-07 22:02:00 -0500

    fix dependency error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import Navbar from './components/Navbar'
-import LogoClouds from './components/LogoCloud'
+import LogoClouds from './components/logoCloud'
 import FooterProductPage from './components/FooterProductPage'
-import HeroSection from './components/HeroSection'
+import HeroSection from './components/heroSection'
 import Testimonial from './components/Testimonial'
 
 
