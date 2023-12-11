40eb0a67594e02988cd7ff76b597e6e48d38e7f2
Hero Emenalom <heroemenalom73@gmail.com>
2023-12-11 09:50:41 -0500

    center 'apply button' in /jobs/listing

--- src/app/(pages)/jobs/listing/JobPanel.tsx
+++ src/app/(pages)/jobs/listing/JobPanel.tsx
@@ -12,8 +12,8 @@ interface Props {
 const JobPanel = ({ id,title }: Props) => (
     <>
         <div className="flex justify-around items-center basis-[150px] w-[70%] border border-gray-200 rounded-md ">
-            <p className="text-2xl font-medium text-sky-500 p-2 text-center ">{title}</p>
-            <Link href={`/jobs/view/${id}`}>
+            <p className=" grow text-2xl font-medium text-sky-500 p-2 text-center ">{title}</p>
+            <Link className="basis-[400px] text-center " href={`/jobs/view/${id}`}>
 
                 <button className="border h-10  border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white font-medium justify-self-end rounded-md w-[100px]">Apply</button>
 
