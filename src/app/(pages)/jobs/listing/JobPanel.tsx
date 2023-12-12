d11616fa1de1aa886afa19e3a5ec7b22f9101c02
Hero Emenalom <heroemenalom73@gmail.com>
2023-12-11 21:14:36 -0500

    configure JobPanel.tsx styles for mobile support

--- src/app/(pages)/jobs/listing/JobPanel.tsx
+++ src/app/(pages)/jobs/listing/JobPanel.tsx
@@ -24,12 +24,12 @@ const JobPanel = ({ id,title }: Props) => {
         const onEnter = () => setMouseOver(true)
         const onLeave = () => setMouseOver(false)
 
-        applyBtn.current?.addEventListener('mouseenter', onEnter)
-        applyBtn.current?.addEventListener('mouseleave', onLeave)
+        applyBtn.current?.addEventListener('pointerenter', onEnter)
+        applyBtn.current?.addEventListener('pointerleave', onLeave)
 
         return () => {
-            applyBtn.current?.removeEventListener('mouseenter',onEnter)
-            applyBtn.current?.removeEventListener('mouseleave',onLeave)
+            applyBtn.current?.removeEventListener('pointerenter',onEnter)
+            applyBtn.current?.removeEventListener('pointerleave',onLeave)
         }
     },[])
 
@@ -41,10 +41,10 @@ const JobPanel = ({ id,title }: Props) => {
     return (
         <>
             <div ref={containerDiv} onClick={handleClick} className="flex justify-around items-center basis-[150px] w-[70%] border border-gray-200 rounded-md hover:cursor-pointer hover:scale-105 transition-transform">
-                <p className=" grow text-2xl font-medium text-sky-500 p-2 text-center ">{title}</p>
+                <p className=" grow text-xl md:text-2xl font-medium text-sky-500 p-2 text-center ">{title}</p>
                 <Link className="basis-[400px] text-center " href={`/jobs/view/${id}`}>
 
-                    <button ref={applyBtn} className="border h-10  border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white font-medium justify-self-end rounded-md w-[100px]">Apply</button>
+                    <button ref={applyBtn} className="border h-8 md:h-10  border-sky-500 text-sky-500 hover:bg-sky-500 hover:text-white font-medium justify-self-end rounded-md w-[80px] md:w-[100px]">Apply</button>
 
                 </Link>
             </div>
