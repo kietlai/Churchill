e12da2f8c92af12c1b936a2842dd87131d12483b
Hero Emenalom <heroemenalom73@gmail.com>
2024-01-22 14:05:53 -0500

    banner change

--- src/app/components/BannerNotif.tsx
+++ src/app/components/BannerNotif.tsx
@@ -24,12 +24,12 @@ export default function Notif() {
         <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8 ">
           <div className="z-64 pointer-events-auto flex items-center justify-between gap-x-6 backdrop-blur-sm drop-shadow-lg bg-slate-200/50 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pl-4 sm:pr-3.5">
             <p className="text-sm leading-6 text-black">
-              <a href="/fbla">
+              <a href="https://github.com/kietlai/Churchill/" target="_blank">
                 <strong className="font-semibold"></strong>
                 <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
                   <circle cx={1} cy={1} r={1} />
                 </svg>
-                Join us at Hereford Jan 22, 2024 for the FBLA competition see what’s coming next&nbsp;<span aria-hidden="true">&rarr;</span>
+                Join us at Hereford Jan 22, 2024 for the FBLA competition. See the code here.&nbsp;<span aria-hidden="true">&rarr;</span>
               </a>
             </p>
             <button onClick={() => setIsShowing((isShowing)=> !isShowing)} type="button" className="-m-1.5 flex-none p-1.5">
